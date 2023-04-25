0d21f05444003cbc8ab8ed077e68c59928340ac0
Heba Omar <jtvonline@yahoo.com>
2023-04-25 22:25:06 +0300

    a function that produces output according to a format

--- printf.c
+++ printf.c
@@ -39,6 +39,10 @@ int _printf(const char *format, ...)
 	{
 		return (-1);
 	}
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+	{
+		return (-1);
+	}
 
 	va_start(args, format);
 	while (format && format[i] != '\0')
