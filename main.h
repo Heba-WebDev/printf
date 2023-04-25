ce1e587ba0b5bcbf86907cdc74b4fa40ac875fb2
UbaidaNA <ubnapari@gmail.com>
2023-04-25 12:49:48 -0700

    Header File

--- main.h
+++ main.h
@@ -7,7 +7,7 @@
 #include <unistd.h>
 #include <limits.h>
 
-int printf(const char *format, ...);
+int _printf(const char *format, ...);
 int handle_strings(int char_printed_count, va_list args, char type);
 
 #endif
